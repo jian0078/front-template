aa221bfa85f03b805e879834d6818d2a6664d3ea
Jane <jane.jiang1023@gmail.com>
2018-02-13 16:24:08 +0000

    Almost finished

--- test.php
+++ test.php
@@ -1,2 +1,242 @@
-This is Git TEST
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+   <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Raleway:700" rel="stylesheet">
+
+    <title>Tinking Monkey</title>
+    <style>
+    	.head-logo{
+    		width:350px;
+
+    	}
+    	.nav-link{
+    		font-family: 'Raleway', sans-serif;
+    		letter-spacing: 3px;
+    		font-size:12px;
+    		color:#666666;
+    	}
+
+    	video{
+    		width:100%;
+    	}
+    	.video-part{
+    		height:70vh;
+    		overflow-y:hidden;
+    	}
+    	.info-p{
+		font-family: 'Lato', sans-serif;
+		font-size:26px;
+		line-height:50px;
+    	}
+    	.w-75{
+    		width:75s%;
+    	}
+
+    	.img-grid{
+    		height:20rem;
+    		
+    		background-repeat: no-repeat;
+    		background-size: cover;
+    		background-position:center center;
+border-left: 1px solid white;
+border-bottom: 1px solid white;
+    	}
+    	.guodu{
+    		/*height:32rem;*/
+    		background-image: linear-gradient(135deg, rgb(249, 237, 179) 0%, rgb(255, 204, 204) 100%);
+
+    	}
+    </style>
+  </head>
+  <body>
+   <div class="container">
+	   	<img src="http://www.tinkeringmonkey.com/wp-content/uploads/2017/07/tm-retina-logo.png" alt="" class="head-logo text-center my-4 mx-auto d-block">
+
+	   	<ul class="nav nav-pills nav-fill">
+		  <li class="nav-item">
+		    <a class="nav-link " href="#">CHOOSE YOUR SIGN</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">EXPLORE</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">GET A QUOTE</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">PORTFOLIO</a>
+
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">CUSTOMERS</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">INSTAGRAM</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">ABOUT</a>
+		</li>
+		</ul>
+   </div>
+
+   <div class="container-fluid px-0 video-part my-4">
+	   	<video poster="http://www.tinkeringmonkey.com/wp-content/uploads/2014/09/hero-lowres.jpg" controls="controls" muted="muted" preload="auto" loop="true" autoplay="true">
+	   		<source type="video/mp4" src="http://www.tinkeringmonkey.com/wp-content/uploads/2014/09/homepage_v1e.mp4">
+		</video>
+
+   	</div>	
+
+   	<div class="container mt-5 py-4">	
+   		<div class="w-75 mx-auto text-center py-5">
+   			<p class="my-2 info-p">	We handcraft wood and metal signs that are built to last. From modern to rustic, we’ll build a sign your whole team will love.
+   			</p>	
+   		</div>
+
+		<div class="row">
+
+
+
+			<div class="col-4">	
+				<div class="simg text-center">
+					<img src="	http://www.tinkeringmonkey.com/wp-content/uploads/2014/09/bear-100px.png"
+					alt="" >
+				</div>	
+
+				<div class="stitl">
+					<p class="text-center my-4" style="font-weight:700">	HANDMADE IN CALIFORNIA</p>
+				</div>
+
+				<div class="scontant">	
+					<p class="text-center" style="font-weight:400">	Our signs are shipped worldwide from our small studio in sunny Berkeley, California. We believe in building only the best and sweating the details.</p>
+				</div>
+			</div>
+
+			<div class="col-4">	
+				<div class="simg text-center">
+					<img src="	http://www.tinkeringmonkey.com/wp-content/uploads/2014/09/trees-100px.png"
+					alt="" >
+				</div>	
+
+				<div class="stitl">
+					<p class="text-center my-4" style="font-weight:700">	NATURAL MATERIALS</p>
+				</div>
+
+				<div class="scontant">	
+					<p class="text-center" style="font-weight:400">	Our obsession with the beauty of natural materials means we only use hand-selected, sustainable materials to get the best results possible.</p>
+				</div>
+			</div>
+
+			<div class="col-4">	
+				<div class="simg text-center">
+					<img src="	http://www.tinkeringmonkey.com/wp-content/uploads/2014/09/heart-100px.png"
+					alt="" >
+				</div>	
+
+				<div class="stitl">
+					<p class="text-center my-4" style="font-weight:700">	GREAT EXPERIENCE</p>
+				</div>
+
+				<div class="scontant">	
+					<p class="text-center" style="font-weight:400">	From ordering to installation, we’ve thought of everything to ensure you receive a sign your team will love. We’re with you every step of the way.</p>
+				</div>
+			</div>
+		</div>
+	</div>	
+
+	<div class="container-fluid px-0">
+		<div class="row">
+
+			<div class="col-4 px-0">	
+				<div class="img-grid" style="background-image:url('http://www.tinkeringmonkey.com/wp-content/uploads/bfi_thumb/drift-mike-2y340vyclw3mzp8azfv3t6.jpg' );"></div>
+			</div>	
+		
+			<div class="col-4 px-0">	
+				<div class="img-grid" style="background-image:url('http://www.tinkeringmonkey.com/wp-content/uploads/bfi_thumb/Screen-Shot-2013-10-07-at-5.44.05-PM-2y310q7il0p4octrubludm.png');"></div>
+			</div>	
+	
+
+			<div class="col-4 px-0">	
+				<div class="img-grid" style="background-image:url('http://www.tinkeringmonkey.com/wp-content/uploads/bfi_thumb/hammock-2y3j9skbfxamh486gbml8q.jpg' );"></div>
+			</div>	
+		</div>
+
+		<div class="row">
+
+			<div class="col-4 px-0">	
+				<div class="img-grid" style="background-image:url('http://www.tinkeringmonkey.com/wp-content/uploads/bfi_thumb/final-5-2ya1elj6j7fwrr0e0gt24q.jpg' );"></div>
+			</div>	
+		
+			<div class="col-4 px-0">	
+				<div class="img-grid" style="background-image:url('http://www.tinkeringmonkey.com/wp-content/uploads/bfi_thumb/drift-mike-2y340vyclw3mzp8azfv3t6.jpg' );"></div>
+			</div>	
+	
+
+			<div class="col-4 px-0">	
+				<div class="img-grid" style="background-image:url('http://www.tinkeringmonkey.com/wp-content/uploads/bfi_thumb/drift-mike-2y340vyclw3mzp8azfv3t6.jpg' );"></div>
+			</div>	
+		</div>
+		
+
+<div class="row">
+
+			<div class="col-4 px-0">	
+				<div class="img-grid" style="background-image:url('http://www.tinkeringmonkey.com/wp-content/uploads/bfi_thumb/drift-mike-2y340vyclw3mzp8azfv3t6.jpg' );"></div>
+			</div>	
+		
+			<div class="col-4 px-0">	
+				<div class="img-grid" style="background-image:url('http://www.tinkeringmonkey.com/wp-content/uploads/bfi_thumb/drift-mike-2y340vyclw3mzp8azfv3t6.jpg' );"></div>
+			</div>	
+	
+
+			<div class="col-4 px-0">	
+				<div class="img-grid" style="background-image:url('http://www.tinkeringmonkey.com/wp-content/uploads/bfi_thumb/drift-mike-2y340vyclw3mzp8azfv3t6.jpg' );"></div>
+			</div>	
+		</div>
+		
+
+</div>	
+
+<div class="container-fluid px-0">
+	<div class="guodu">
+		<div class="row">
+			<div class="col-6 col-md-6 col-xl-4 offfset-xl-2">
+				<h2 style="font-family: Raleway; font-size:23px; letter-spacing: 8px; " class="text-center my-5 pt-5 pb-3">SIMPLE ORDERING</h2>
+				<p style="font-family: Lato, Arial, Helvetica, sans-serif; font-size:18px; line-height:34px" class="text-center pb-2">Fill out a quote request form and receive a custom order page within 24 hours that includes timeline, pricing, and even a mockup if you submit artwork. Order directly from your custom order page, or simply forward the link to accounting.</p>
+				<div style="display:flex; justy-content:center; align-items:center">
+				<button href="#" class="btn btn-danger mx-auto mb-5">GET STARTED</button>
+				</div>
+			</div>
+		<div class="col-6 col-xl-4 offfset-xl-2">
+			<div style="background-image:url(http://www.tinkeringmonkey.com/wp-content/uploads/2014/09/metalab.jpg);background-size:cover;background-position:center,center;height: 20rem" class="mt-5"></div>
+			
+		</div>
+	</div>
+</div>
+
+
+<div class="container-fluid px-0">
+	<div class="fix-bg" style="background-image:url(http://www.tinkeringmonkey.com/wp-content/uploads/2014/09/moore-process-1-lowestres.jpg);background-attachment:fixed;height:50vh; background-position:top center; ">
+		
+	</div>
+</div>
+
+<div style="height: 100vh">
+	
+</div>
+<a href="#" class="btn btn-sucess" style="position:fixed; bottom:15px; right:30px; background-color: #78A300;border-radius:25px" class="px-5">Help
+</a>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
 
